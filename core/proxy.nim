43d991990ca2aebb06657635fdcbe076bcd17ce9
Airscript <francesco@airscript.it>
2022-12-29 00:39:24 +0100

    refactor(core): add listionary inside proxy module

--- core/proxy.nim
+++ core/proxy.nim
@@ -1,11 +1,12 @@
 import std/tables
 
 include ../functions/help
+include ../interfaces/common
 include ../functions/validate
 
-proc proxy(parameters: OrderedTable[int, OrderedTable[string, string]]) =
-  for parameter in parameters.values:
-    case parameter["key"]
+proc proxy(commands: Listionary) =
+  for command in commands.values:
+    case command["key"]
       of "validate":
         validate()
         break
