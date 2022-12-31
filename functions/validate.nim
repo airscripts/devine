5d3eeb8b7044986a0b2ac326ad1f16d144bf3d70
Airscript <francesco@airscript.it>
2022-12-31 17:53:45 +0100

    feat(functions): add specs procedure

--- functions/validate.nim
+++ functions/validate.nim
@@ -1,15 +1,25 @@
 import std/tables
 
+from strformat import fmt
+from std/json import JsonNode, parseJson
 from ../types/index as types import Listionary
 from ../utils/index as utils import hasArgument
 
-proc specs(): void =
-  echo "specs procedure."
+proc specs(filename: string): JsonNode =
+  try:
+    var folder: string = "specs/"
+    var extension: string = ".json"
+    var spec: string = readFile(fmt"{folder}{filename}{extension}")
+    return parseJson(spec)
+
+  except IOError:
+    echo "Spec not available."
 
 proc custom(): void =
   echo "custom procedure."
 
 proc validate*(args: Listionary): void =
+  var spec: JsonNode
   var path: OrderedTableRef[string, string]
   var length: int = len(args)
 
@@ -20,7 +30,7 @@ proc validate*(args: Listionary): void =
     case arg["key"]:
       of "spec":
         if cast[bool](arg["value"]):
-          specs()
+          spec = specs(arg["value"])
           echo "--spec option applied;"
 
       of "custom":
