3ba49f0e2afcd21c796d9767e191bb8293ffe5de
Airscript <francesco@airscript.it>
2022-12-31 16:57:37 +0100

    feat(utils): add hasArgument util

---
+++ utils/hasArgument.nim
@@ -0,0 +1,8 @@
+import std/tables
+
+proc hasArgument*(value: OrderedTableRef[string, string]): bool =
+  if value["type"] == "argument":
+    return true
+
+  else:
+    return false
---
+++ utils/index.nim
@@ -0,0 +1,2 @@
+import hasArgument
+export hasArgument
