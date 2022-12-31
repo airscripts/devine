b5d5423888ae3bda69cab8c583ad16ecfd6832c4
Airscript <francesco@airscript.it>
2022-12-31 18:42:10 +0100

    refactor: collapse initOptParser line to one line

--- main.nim
+++ main.nim
@@ -4,9 +4,6 @@ from std/parseopt import initOptParser
 from core/proxy import proxy
 from core/parser import parser
 
-var opts = initOptParser(
-  commandLineParams()
-)
-
+var opts = initOptParser(commandLineParams())
 let args = parser(opts)
 proxy(args)
