1d348cd290e913e8c2cca4352c12d5c3acf8ec16
Airscript <francesco@airscript.it>
2023-01-02 00:03:39 +0100

    refactor(core): add kwargs in proxy module

--- core/proxy.nim
+++ core/proxy.nim
@@ -6,10 +6,10 @@ from ../constants/index as constants import PROXY_KEYS, PARAMETER_KEYS
 
 proc proxy*(args: Listionary): void =
   var arg: Dictionary
-  discard args.pop(0, arg)
+  discard pop(t=args, key=0, val=arg)
 
   case arg[PARAMETER_KEYS.key]
     of PROXY_KEYS.help: commands.help()
     of PROXY_KEYS.support: commands.support()
-    of PROXY_KEYS.validate: commands.validate(args)
+    of PROXY_KEYS.validate: commands.validate(args=args)
     else: commands.help()
