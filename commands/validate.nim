e4461e78689c34c2e18042b548d1e06354e061a2
Airscript <francesco@airscript.it>
2023-01-01 21:45:54 +0100

    feat(commands): add processor and checker procedures

--- commands/validate.nim
+++ commands/validate.nim
@@ -1,11 +1,19 @@
 import std/tables
 
 from strformat import fmt
-from std/json import JsonNode, parseJson
 from ../types/index as types import Listionary
 from ../utils/index as utils import hasArgument
+from std/json import JsonNode, parseJson, contains, keys
 from ../constants/index as constants import VALIDATE_OPTIONS
-from ../errors/index as errors import SPEC_NOT_AVAILABLE, CUSTOM_SPEC_NOT_FOUND
+
+from ../errors/index as errors import 
+  MISSING_SPEC,
+  MULTIPLE_SPECS,
+  MISSING_STRUCTURE,
+  SPEC_NOT_AVAILABLE,
+  STRUCTURE_NOT_FOUND,
+  CUSTOM_SPEC_NOT_FOUND,
+  STRUCTURE_MATCH_ERROR
 
 proc specs(filename: string): JsonNode =
   try:
@@ -16,6 +24,7 @@ proc specs(filename: string): JsonNode =
 
   except IOError:
     echo SPEC_NOT_AVAILABLE
+    system.quit(QuitFailure)
 
 proc custom(path: string): JsonNode =
   try:
@@ -24,29 +33,72 @@ proc custom(path: string): JsonNode =
 
   except IOError:
     echo CUSTOM_SPEC_NOT_FOUND
+    system.quit(QuitFailure)
+
+proc hasMultipleSpecs(args: Listionary): bool =
+  var custom: bool
+  var standard: bool
+
+  for arg in args.values:
+    case arg["key"]
+      of "spec": standard = true
+      of "custom": custom = true
+
+  if standard and custom: return true
+  else: return false
+
+proc parser(arg: OrderedTableRef[string, string]): JsonNode =
+  case arg["key"]:
+    of VALIDATE_OPTIONS.spec:
+      if cast[bool](arg["value"]):
+        return specs(arg["value"])
+
+    of VALIDATE_OPTIONS.custom:
+      if cast[bool](arg["value"]):
+        return custom(arg["value"])
+    
+    else:
+      echo MISSING_SPEC
+      system.quit(QuitFailure)
+
+proc processor(path: OrderedTableRef[string, string]): JsonNode =
+  try:
+    let structure: string = readFile(path["key"])
+    return parseJson(structure)
+
+  except IOError:
+    echo STRUCTURE_NOT_FOUND
+
+proc checker(structure: JsonNode, spec: JsonNode): void =
+  for key in keys(structure):
+    if not contains(spec, key):
+      raise newException(ValueError, "Key not found")
 
 proc validate*(args: Listionary): void =
   var spec: JsonNode
+  var structure: JsonNode
   var path: OrderedTableRef[string, string]
   let length: int = len(args)
 
   if utils.hasArgument(args[length]):
     discard args.pop(length, path)
 
+  if hasMultipleSpecs(args):
+    echo MULTIPLE_SPECS
+    system.quit(QuitFailure)
+
   for arg in args.values:
-    case arg["key"]:
-      of VALIDATE_OPTIONS.spec:
-        if cast[bool](arg["value"]):
-          spec = specs(arg["value"])
-          echo "--spec option applied;"
+    spec = parser(arg)
 
-      of VALIDATE_OPTIONS.custom:
-        if cast[bool](arg["value"]):
-          spec = custom(arg["value"])
-          echo "--custom option applied;"
-      
-      else:
-        echo fmt"-{arg[""key""]} option skipped;"
+  if not cast[bool](path):
+    echo MISSING_STRUCTURE
+    system.quit(QuitFailure)
 
-  if cast[bool](path):
-    echo path
+  try:
+    structure = processor(path)
+    checker(structure, spec)
+    system.quit(QuitSuccess)
+  
+  except ValueError:
+    echo STRUCTURE_MATCH_ERROR
+    system.quit(QuitFailure)
--- errors/commands.nim
+++ errors/commands.nim
@@ -1,2 +1,7 @@
+const MISSING_SPEC*: string = "Missing specification."
+const MISSING_STRUCTURE*: string = "Missing structure."
 const SPEC_NOT_AVAILABLE*: string = "Spec not available."
 const CUSTOM_SPEC_NOT_FOUND*: string = "Custom spec not found."
+const STRUCTURE_NOT_FOUND*: string = "Structure file not found."
+const MULTIPLE_SPECS*: string = "Multiple specs have been given."
+const STRUCTURE_MATCH_ERROR*: string = "An unexpected key has been found in the given structure."
